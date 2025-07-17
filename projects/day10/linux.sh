0e3cee52334dad4897cbcae1710d6844e025f8a1
Muhammad Huzaifa Shahbaz <kinghuzaifa.shahbaz@gmail.com>
2025-07-17 15:07:47 +0500

    Create linux.sh

---
+++ projects/day10/linux.sh
@@ -0,0 +1,3 @@
+sudo apt install python3-pip
+sudo apt install python3-dev build-essential
+python3 -m pip install --user streamlit PyPDF2 python-docx langchain langchain-openai langchain-huggingface langchain-community python-dotenv streamlit-chat faiss-cpu sentence-transformers
